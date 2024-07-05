7b00ec1e083a83d3641f5e710d7684ef0d810661
Joshua Berbie <143854567+Uwa02@users.noreply.github.com>
2024-07-05 11:46:50 +0800

    Update file1.py

--- file1.py
+++ file1.py
@@ -2,8 +2,9 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n-5)
+        return n * factorial(n-1)
 
+#this is the change
 result = factorial(5)
 print(f"Factorial of 5 is: {result}")
 
